c170baaec871075caad01dc2edf1a810104f5ea9
alex <alex>
2016-09-28 15:49:07 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="html_wrapper",
+      version="0.1",
+      description="Html Parser with lxml backend. Implements subset of BeautifulSoup API",
+      url="",
+      author="thismachinechills (Alex)",
+      license="AGPL 3.0",
+      packages=['html_wrapper'],
+      zip_safe=True)
