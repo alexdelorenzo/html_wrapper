2f5dc06e14b1f5d98aaf39514f723a88f3c3c011
alex <alex>
2016-10-07 21:10:25 -0400

    Add requirments and keywords

--- setup.py
+++ setup.py
@@ -8,4 +8,6 @@ setup(name="html_wrapper",
       license="AGPL 3.0",
       packages=['html_wrapper'],
       zip_safe=True,
-      install_requires=["lxml"])
+      install_requires=["lxml"],
+      keywords = "html parser wrapper beautifulsoup bs4 lxml xml html fast xpath".split(),
+      )
