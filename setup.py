0d14b52532f63e5da458cded0f7847d03b61d107
alex <alex>
2016-10-07 20:59:33 -0400

    Update requirements

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
 setup(name="html_wrapper",
-      version="0.1",
+      version="0.1.1",
       description="Html Parser with lxml backend. Implements subset of BeautifulSoup API",
       url="https://github.com/thismachinechills/html_wrapper",
       author="thismachinechills (Alex)",
       license="AGPL 3.0",
       packages=['html_wrapper'],
-      zip_safe=True)
+      zip_safe=True,
+      install_requires=["lxml"])
