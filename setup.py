199957d8079d614ad4c30d88637e959267352d6d
alexdelorenzo <you@example.com>
2021-01-03 00:50:00 -0500

    Spacing

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 
 
-setup(name="html_wrapper",
-      version="0.3.2",
-      description="HTML parser with an lxml backend. Implements a subset of BeautifulSoup API and is an order of magnitude faster",
-      url="https://github.com/thismachinechills/html_wrapper",
-      author="thismachinechills (Alex)",
-      license="AGPL 3.0",
-      packages=['html_wrapper'],
-      zip_safe=True,
-      install_requires=["lxml"],
-      keywords="html parser wrapper beautifulsoup bs4 lxml xml html fast xpath".split(),
-      )
+setup(
+  name="html_wrapper",
+  version="0.3.2",
+  description="HTML parser with an lxml backend. Implements a subset of BeautifulSoup API and is an order of magnitude faster",
+  url="https://github.com/thismachinechills/html_wrapper",
+  author="thismachinechills (Alex)",
+  license="AGPL 3.0",
+  packages=['html_wrapper'],
+  zip_safe=True,
+  install_requires=["lxml"],
+  keywords="html parser wrapper beautifulsoup bs4 lxml xml html fast xpath".split(),
+)
